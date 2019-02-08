703176200ec5e860f3cb925e5870df1654a1635e
Billy Hoang <billy-hoang@hotmail.com>
2019-02-08 01:47:50 -0800

    switched to semantic ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import RepositoryList from './components/RepositoryList';
-import { Button } from 'antd';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <RepositoryList/>
-        <Button type="primary">Button</Button>
       </div>
     );
   }
